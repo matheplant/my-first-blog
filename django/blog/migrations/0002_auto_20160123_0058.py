d2f884b649f202c78a3a506896760d38f9df22eb
Your Name <you@example.com>
2016-01-23 01:33:35 +0900

    My Django app, 첫번째 커밋

---
+++ django/blog/migrations/0002_auto_20160123_0058.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('blog', '0001_initial'),
+    ]
+
+    operations = [
+        migrations.RenameField(
+            model_name='post',
+            old_name='create_date',
+            new_name='created_date',
+        ),
+        migrations.RenameField(
+            model_name='post',
+            old_name='titile',
+            new_name='title',
+        ),
+    ]
